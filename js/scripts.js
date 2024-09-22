09eb01908525df7cef00a84170dff93e1fc532c1
jjvnz-dev <jjvnz.dev@outlook.com>
2024-09-21 20:53:00 -0500

    Update UI/UX

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,28 @@
-import gsap from "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js";
-import ScrollTrigger from "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js";
-
+import gsap from "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js";
+import { ScrollTrigger } from "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"; // Cambia la versión a la misma que gsap
 
 document.addEventListener('DOMContentLoaded', () => {
-  gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollTrigger);
 
-  gsap.utils.toArray(".section").forEach(section => {
-    gsap.fromTo(section,
-      { opacity: 0, y: 50 },
-      {
-        opacity: 1,
-        y: 0,
-        duration: 1,
-        scrollTrigger: {
-          trigger: section,
-          start: "top 80%",
-          end: "bottom top",
-          scrub: true,
-          toggleActions: "play none none reverse"
-        }
-      }
-    );
-  });
+    gsap.utils.toArray(".section").forEach(section => {
+        gsap.fromTo(section,
+            { opacity: 0, y: 50 },
+            {
+                opacity: 1,
+                y: 0,
+                duration: 1,
+                scrollTrigger: {
+                    trigger: section,
+                    start: "top 80%",
+                    end: "bottom top",
+                    scrub: true,
+                    toggleActions: "play none none reverse"
+                }
+            }
+        );
+    });
 
-  // Animación para el botón (opcional)
-  document.querySelector('.button').addEventListener('click', () => {
-    gsap.to(window, { duration: 1, scrollTo: "#work-showcase" });
-  });
+    document.querySelector('.button').addEventListener('click', () => {
+        gsap.to(window, { duration: 1, scrollTo: "#work-showcase" });
+    });
 });
